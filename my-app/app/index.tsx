5b3a6be385a5dbfde7111eabda67b24ab26333e8
Nathan <georgenathan010@gmail.com>
2024-05-05 13:23:32 +0300

    Added the hero section in the onboarding page

--- my-app/app/index.tsx
+++ my-app/app/index.tsx
@@ -17,6 +17,24 @@ export default function App() {
         className='w-[130px] h-[84px]'
         resizeMode='contain'
         />
+        <Image
+        source={images.cards}
+        className='max-w-[380px] w-full h-[300px]'
+        resizeMode='contain'
+        />
+
+        <View className='relative mt-5'>
+        <Text className='text-3xl text-white font-bold text-center'>
+          Discover endless possibilites with <Text className='text-secondary-200'>Aora</Text>
+        </Text>
+        <Image
+        source={images.path}
+        className='w-[136px] h-[15px] absolute -bottom-3 -right-8'
+        resizeMode='contain'
+        />
+        </View>
+
+        <Text className='text-sm font-pregular text-gray-100 mt-7 text-center'>Set out on a journey of exciting ventures with our app. Aora</Text>
         </View>
       </ScrollView>
 
